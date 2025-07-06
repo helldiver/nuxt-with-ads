705d88234a9d0fbd5b542182bceea063c3dd9cac
HellDiver <helldiver.taiwan@gmail.com>
2025-07-06 17:03:19 +0800

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  future: { compatibilityVersion: 4 },
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui']
+})
